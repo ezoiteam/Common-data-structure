42f03d3452fc10cf59d7dda4a1f1578366bd99bc
Kelo <ghKelo@users.noreply.github.com>
2017-08-17 16:12:18 +0800

    Update Tree-array.cpp

--- Tree-array.cpp
+++ Tree-array.cpp
@@ -6,27 +6,27 @@ struct bit
 	int c[n];
 	int lowbit(int x)
 	{
-    	return x & (-x);
+		return x & (-x);
 	}
 
 	void modify(int x,int y)
 	{
-    	while(x <= n)
+		while(x <= n)
 		{
-        	c[x] += y;
-        	x += lowbit(x);
-    	}
+			c[x] += y;
+			x += lowbit(x);
+		}
 	}
 
 	int query(int x)
 	{
-    	int ans = 0;
-    	while(x > 0)
+		int ans = 0;
+		while(x > 0)
 		{
-        	ans += c[x];
-        	x -= lowbit(x);
-    	}
-      	return ans;
+			ans += c[x];
+			x -= lowbit(x);
+		}
+		return ans;
 	}
 };
 #undef n
