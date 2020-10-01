b9a5454719b855d6fc3aa86b379c433b6dea26e6
Aniket <43602527+masteranimax@users.noreply.github.com>
2020-10-01 21:31:16 +0530

    Update Tree-array.cpp
    Added comments

--- Tree-array.cpp
+++ Tree-array.cpp
@@ -1,15 +1,15 @@
 #ifndef __bit__
 #define __bit__
 #define n 10005
-struct bit
+struct bit    // Binary Indexed Tree(or fenwick tree) 1 based index implementation
 {
-	int c[n];
-	int lowbit(int x)
+	int c[n];    // array of size n
+	int lowbit(int x)   // returns smallest set bit
 	{
-		return x & (-x);
+		return x & (-x); 
 	}
 
-	void modify(int x,int y)
+	void modify(int x,int y)     // Add value y at index x if array
 	{
 		while(x <= n)
 		{
@@ -18,9 +18,9 @@ struct bit
 		}
 	}
 
-	int query(int x)
+	int query(int x)   // prefix sum of array upto index x
 	{
-		int ans = 0;
+	        int ans = 0;
 		while(x > 0)
 		{
 			ans += c[x];
